c1873d96f4752b2fa8ec11082ff4e33411e58252
Robert Denus <denus.robert@gmail.com>
2020-03-08 13:34:18 +0100

    init, add ant-desing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'antd/es/button';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
