fdb65dcab4749a7677bcf2d52e6b9d2c7b4435f2
Robert Denus <denus.robert@gmail.com>
2020-03-12 00:16:37 +0100

    fix form resp, fix currency preselect

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import { Input, InputNumber, Form, Select, DatePicker, TimePicker } from 'antd';
+import {
+  Input,
+  InputNumber,
+  Form,
+  Select,
+  DatePicker,
+  TimePicker,
+  Row,
+  Col
+} from 'antd';
 import { fromExpenseToFormData, fromFormDataToExpense } from '../shared/utils';
 import {
   DATE_FORMAT,
   TIME_FORMAT,
   CURRENCIES,
-  EXPENSE_CATEGORIES
+  EXPENSE_CATEGORIES,
+  DEFAULT_CURRENCY
 } from '../shared/constants';
 
 export const ExpenseForm = ({
@@ -53,46 +63,70 @@ export const ExpenseForm = ({
         </Select>
       </Form.Item>
 
-      <Form.Item
-        name="date"
-        rules={[{ required: true, message: 'Date is required' }]}
-        label="Date"
-        labelAlign="left"
-      >
-        <DatePicker format={DATE_FORMAT} disabled={disabled} />
-      </Form.Item>
-      <Form.Item
-        name="time"
-        rules={[{ required: true, message: 'Time is required' }]}
-        label="Time"
-        labelAlign="left"
-      >
-        <TimePicker format={TIME_FORMAT} disabled={disabled} />
-      </Form.Item>
+      <Row gutter={32}>
+        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          <Form.Item
+            name="date"
+            rules={[{ required: true, message: 'Date is required' }]}
+            label="Date"
+            labelAlign="left"
+          >
+            <DatePicker
+              format={DATE_FORMAT}
+              disabled={disabled}
+              style={{ width: '100%' }}
+            />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          <Form.Item
+            name="time"
+            rules={[{ required: true, message: 'Time is required' }]}
+            label="Time"
+            labelAlign="left"
+          >
+            <TimePicker
+              format={TIME_FORMAT}
+              disabled={disabled}
+              style={{ width: '100%' }}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="amount"
-        rules={[{ required: true }, { type: 'number', min: 0 }]}
-        label="Amount"
-        labelAlign="left"
-      >
-        <InputNumber disabled={disabled} />
-      </Form.Item>
-
-      <Form.Item
-        name="currency"
-        label="Currency"
-        labelAlign="left"
-        rules={[{ required: true, message: 'Currency is required' }]}
-      >
-        <Select disabled={disabled}>
-          {CURRENCIES.map(currency => (
-            <Select.Option key={currency} value={currency}>
-              {currency}
-            </Select.Option>
-          ))}
-        </Select>
-      </Form.Item>
+      <Row gutter={32}>
+        <Col xs={24} sm={8} md={8} lg={8} xl={8}>
+          <Form.Item
+            label="Currency"
+            name="currency"
+            rules={[{ required: true, message: 'Currency is required' }]}
+          >
+            <Select
+              disabled={disabled}
+              defaultValue={expense?.currency || DEFAULT_CURRENCY}
+              style={{ minWidth: 100 }}
+            >
+              {CURRENCIES.map(currency => (
+                <Select.Option key={currency} value={currency}>
+                  {currency}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={16} md={16} lg={16} xl={16}>
+          <Form.Item
+            label="Amount"
+            name="amount"
+            rules={[
+              { required: true, message: 'Amount is required' },
+              { type: 'number', min: 0 }
+            ]}
+          >
+            <InputNumber style={{ width: '100%' }} disabled={disabled} />
+          </Form.Item>
+        </Col>
+      </Row>
     </Form>
   );
 };
